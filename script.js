3146531e1c8f82d6a3633420fd998d49f993f7e9
mishael-codes <enyimishael22@gmail.com>
2023-08-03 13:42:04 +0100

    bug fixes
    Fixed a bug that didn't allow the menu to be displayed when called.
    
    Also removed irrelevant links from the HTML file

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+let ham = document.querySelector(".ham");
 let bar1 = document.querySelector(".bar1");
 let bar2 = document.querySelector(".bar2");
 let navList = document.querySelector(".navlist");
 let nav = document.querySelector(".nav");
+
+ham.addEventListener("click", showMenu);
 
 function showMenu() {
   bar1.classList.toggle("openBar1");
